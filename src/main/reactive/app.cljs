a839451a5647f161b55ac427318577828859473f
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2019-12-20 21:30:05 -0300

    adiciona novo arquivo

--- src/main/reactive/app.cljs
+++ src/main/reactive/app.cljs
@@ -1,6 +1,9 @@
 (ns reactive.app
-  (:require [reactive.rxjs :refer [move-mouse$]]))
+  (:require [reactive.rxjs :refer [move-mouse$]]
+            [reactive.graph :refer [init-graph]]))
 
+; https://observablehq.com/d/42f72efad452c2f0
 (defn init []
+  (init-graph)
   (move-mouse$))
 
---
+++ src/main/reactive/graph.cljs
@@ -0,0 +1,6 @@
+(ns reactive.graph
+  (:require ["d3" :as d3]))
+
+(defn init-graph
+  []
+  (js/console.log "Iniciando grafo..."))
