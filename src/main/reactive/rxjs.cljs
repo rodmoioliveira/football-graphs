6958d3b65f8812767a3386439c13fda56c6df5be
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2019-12-20 21:19:55 -0300

    separa rxjs

---
+++ src/main/reactive/rxjs.cljs
@@ -0,0 +1,33 @@
+(ns reactive.rxjs
+  (:require ["rxjs" :as rx]
+            ["rxjs/operators" :as rx-op]))
+
+(def root (-> js/document (.getElementById "root")))
+
+(defn set-div
+  [value]
+  (-> root
+      .-innerHTML
+      (set! value)))
+
+(defn get-coods
+  [e] {:y (.-clientY e) :x (.-clientX e)})
+
+(defn format-str
+  [{:keys [x y]}]
+  (str "posição x: " x ", posição y: " y))
+
+(defn render-pos
+  [e]
+  (-> e
+      get-coods
+      format-str))
+
+; https://cljs.github.io/api/cljs.core/DOT
+(defn move-mouse$
+  []
+  (-> js/document
+      (rx/fromEvent "mousemove")
+      (.pipe (rx-op/map render-pos) (rx-op/startWith "posição x: ???, posição y: ???"))
+      (.subscribe set-div)))
+
