6e6627e48f9584a6ab0e60cc7db27ac3faa88340
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2019-12-27 18:18:44 -0300

    gera edges aleatórios

--- src/main/reactive/graph.cljs
+++ src/main/reactive/graph.cljs
@@ -134,6 +134,14 @@
   [x-% y-%]
   #js {:x (* (.-width canvas) (/ x-% 100))  :y (* (.-height canvas) (/ y-% 100))})
 
+(def mock-edges (for [source ["1" "5" "3" "11" "15" "16" "6" "8" "14" "6" "9" "7"]
+                      target ["1" "5" "3" "11" "15" "16" "6" "8" "14" "6" "9" "7"]
+                      :let [edge {:source source
+                                  :target target
+                                  :value (if (zero? (rand-int 2)) 1 50)}]
+                      :when (not= source target)]
+                  edge))
+
 ; Inspiração:
 ; https://tsj101sports.com/2018/06/20/football-with-graph-theory/
 (def mock-data
@@ -152,44 +160,45 @@
            {:id "5" :group 1 :initial_pos (place-node 28 77)}
            {:id "1" :group 1 :initial_pos (place-node 50 95)}
            ]
-   :links [
-           {:source "6" :target "14" :value 1}
-           {:source "14" :target "6" :value 100}
-           {:source "8" :target "14" :value 1}
-           {:source "14" :target "8" :value 100}
-           {:source "6" :target "1" :value 1}
-           {:source "1" :target "6" :value 100}
-           {:source "3" :target "1" :value 1}
-           {:source "1" :target "3" :value 100}
-           {:source "5" :target "1" :value 23}
-           {:source "1" :target "5" :value 2}
-           {:source "1" :target "7" :value 2}
-           {:source "7" :target "1" :value 2}
-           {:source "1" :target "16" :value 2}
-           {:source "16" :target "1" :value 2}
-           {:source "5" :target "11" :value 2}
-           {:source "11" :target "5" :value 2}
-           {:source "9" :target "11" :value 2}
-           {:source "11" :target "9" :value 2}
-           {:source "6" :target "11" :value 2}
-           {:source "11" :target "6" :value 2}
-           {:source "8" :target "11" :value 2}
-           {:source "11" :target "8" :value 2}
-           {:source "3" :target "11" :value 2}
-           {:source "11" :target "3" :value 2}
-           {:source "7" :target "11" :value 2}
-           {:source "11" :target "7" :value 2}
-           {:source "15" :target "11" :value 2}
-           {:source "11" :target "15" :value 2}
-           {:source "15" :target "8" :value 2}
-           {:source "8" :target "15" :value 2}
-           {:source "15" :target "5" :value 2}
-           {:source "5" :target "15" :value 2}
-           {:source "1" :target "9" :value 2}
-           {:source "9" :target "1" :value 2}
-           {:source "14" :target "9" :value 2}
-           {:source "9" :target "14" :value 2}
-           ]
+   :links (-> mock-edges vec)
+   ; :links [
+   ;         {:source "6" :target "14" :value 1}
+   ;         {:source "14" :target "6" :value 100}
+   ;         {:source "8" :target "14" :value 1}
+   ;         {:source "14" :target "8" :value 100}
+   ;         {:source "6" :target "1" :value 1}
+   ;         {:source "1" :target "6" :value 100}
+   ;         {:source "3" :target "1" :value 1}
+   ;         {:source "1" :target "3" :value 100}
+   ;         {:source "5" :target "1" :value 23}
+   ;         {:source "1" :target "5" :value 2}
+   ;         {:source "1" :target "7" :value 2}
+   ;         {:source "7" :target "1" :value 2}
+   ;         {:source "1" :target "16" :value 2}
+   ;         {:source "16" :target "1" :value 2}
+   ;         {:source "5" :target "11" :value 2}
+   ;         {:source "11" :target "5" :value 2}
+   ;         {:source "9" :target "11" :value 2}
+   ;         {:source "11" :target "9" :value 2}
+   ;         {:source "6" :target "11" :value 2}
+   ;         {:source "11" :target "6" :value 2}
+   ;         {:source "8" :target "11" :value 2}
+   ;         {:source "11" :target "8" :value 2}
+   ;         {:source "3" :target "11" :value 2}
+   ;         {:source "11" :target "3" :value 2}
+   ;         {:source "7" :target "11" :value 2}
+   ;         {:source "11" :target "7" :value 2}
+   ;         {:source "15" :target "11" :value 2}
+   ;         {:source "11" :target "15" :value 2}
+   ;         {:source "15" :target "8" :value 2}
+   ;         {:source "8" :target "15" :value 2}
+   ;         {:source "15" :target "5" :value 2}
+   ;         {:source "5" :target "15" :value 2}
+   ;         {:source "1" :target "9" :value 2}
+   ;         {:source "9" :target "1" :value 2}
+   ;         {:source "14" :target "9" :value 2}
+   ;         {:source "9" :target "14" :value 2}
+   ;         ]
    })
 
 ; https://observablehq.com/d/42f72efad452c2f0
