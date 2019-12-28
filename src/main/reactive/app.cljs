647fcd6ed1ce562f30d700e2140a46530e11395c
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2019-12-28 16:17:20 -0300

    adiciona alguns temas

--- src/main/reactive/app.cljs
+++ src/main/reactive/app.cljs
@@ -1,16 +1,18 @@
 (ns reactive.app
   (:require
-    [reactive.config :refer [config mock-data]]
+    [reactive.config :refer [config mock-data themes]]
     [reactive.graph :refer [force-graph]]))
 
 (def all-canvas (-> js/document
                     (.querySelectorAll ".canvas")
                     array-seq
-                    ((fn [arr] (map #(.getAttribute % "id") arr)))))
+                    (#(map (fn [v] {:id (.getAttribute v "id")
+                                    :theme (-> v (.getAttribute "data-theme") keyword)}) %))))
 
 (defn init []
-  (doseq [id all-canvas]
-    (force-graph {:data (clj->js mock-data) :config (config {:id id})})))
+  (doseq [canvas all-canvas]
+    (force-graph {:data (clj->js mock-data)
+                  :config (config {:id (canvas :id) :theme (-> canvas :theme themes)})})))
 
 (defn ^:dev/after-load start []
   (init))
--- src/main/reactive/config.cljs
+++ src/main/reactive/config.cljs
@@ -4,17 +4,42 @@
     ["d3" :as d3]))
 
 ; ==================================
+; Themes
+; ==================================
+(def themes
+  {
+   :grey {:primary "black"
+          :secondary "white"
+          :edges (-> d3 (.-interpolateGreys))}
+   :blue {:primary "black"
+          :secondary "white"
+          :edges (-> d3 (.-interpolateBlues))}
+   :green {:primary "black"
+          :secondary "white"
+          :edges (-> d3 (.-interpolateGreens))}
+   :orange {:primary "black"
+          :secondary "white"
+          :edges (-> d3 (.-interpolateOranges))}
+   :purple {:primary "black"
+          :secondary "white"
+          :edges (-> d3 (.-interpolatePurples))}
+   :red {:primary "black"
+          :secondary "white"
+          :edges (-> d3 (.-interpolateReds))}
+   })
+
+; ==================================
 ; Configuration hashmap
 ; ==================================
 (defn config
-  [{:keys [id]}]
+  [{:keys [id theme]}]
   (let [mapping {:domains {:passes->color #js [(- 70) 160]
                            :passes->edge-width #js [0 100]}
                  :codomains {:passes<-edge-width #js [2 21]}}
         font {:weight "700"
               :size "22px"
               :type "sans-serif"
-              :color "white"
+              :color (theme :secondary)
               :text-align "center"
               :base-line "middle"}
         node-radius 35]
@@ -27,14 +52,14 @@
      :edges {:padding 10
              :distance-between (/ node-radius 2.2)}
      :nodes {:radius node-radius
-             :fill {:color "black"}
-             :outline {:color "white"
+             :fill {:color (theme :primary)}
+             :outline {:color (theme :secondary)
                        :width "1.5"}
              :font (assoc font :full (str/join " " [(font :weight)
                                                     (font :size)
                                                     (font :type)]))}
      :scales {:edges->colors (-> d3
-                                 (.scaleSequential (-> d3 (.-interpolateGreys)))
+                                 (.scaleSequential (theme :edges))
                                  (.domain (-> mapping :domains :passes->color)))
               :edges->width (-> d3
                                 (.scaleLinear)
@@ -46,8 +71,8 @@
 ; ==================================
 (defn place-node
   [x-% y-%]
-  #js {:x (* (-> (config {:id "canvas0"}) :canvas .-width) (/ x-% 100))
-       :y (* (-> (config {:id "canvas0"}) :canvas .-height) (/ y-% 100))})
+  #js {:x (* (-> (config {:id "canvas0" :theme (-> themes :grey)}) :canvas .-width) (/ x-% 100))
+       :y (* (-> (config {:id "canvas0" :theme (-> themes :grey)}) :canvas .-height) (/ y-% 100))})
 
 (def mock-edges (for [source ["1" "5" "3" "11" "15" "16" "6" "8" "14" "6" "9" "7"]
                       target ["1" "5" "3" "11" "15" "16" "6" "8" "14" "6" "9" "7"]
