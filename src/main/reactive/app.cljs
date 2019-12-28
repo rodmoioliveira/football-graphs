ffb272e335c372bea4638248b91da2106d6564a3
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2019-12-28 15:26:35 -0300

    adiciona múltiplos canvas...

--- src/main/reactive/app.cljs
+++ src/main/reactive/app.cljs
@@ -1,13 +1,16 @@
 (ns reactive.app
   (:require
-    ; [reactive.rxjs :refer [move-mouse$]]
-    [reactive.graph :refer [init-graph]]))
+    [reactive.config :refer [config mock-data]]
+    [reactive.graph :refer [force-graph]]))
 
+(def all-canvas (-> js/document
+                    (.querySelectorAll ".canvas")
+                    array-seq
+                    ((fn [arr] (map #(.getAttribute % "id") arr)))))
 
 (defn init []
-  (init-graph)
-  ; (move-mouse$)
-  )
+  (doseq [id all-canvas]
+    (force-graph {:data (clj->js mock-data) :config (config {:id id})})))
 
 (defn ^:dev/after-load start []
   (init))
