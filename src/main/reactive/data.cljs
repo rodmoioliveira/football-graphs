9b1d3d738d3868a4644908c551c3ae6c091fa623
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-01-03 17:42:13 -0300

    move diretório

--- src/main/reactive/data.cljs
+++ src/main/reactive/data.cljs
@@ -4,13 +4,13 @@
     [reactive.utils :refer [hash-by]]))
 
 (def matches (-> js/JSON
-                 (.parse (rc/inline "./data/matches_World_Cup.json"))
+                 (.parse (rc/inline "../data/matches_World_Cup.json"))
                  (js->clj :keywordize-keys true)
                  ((fn [v] (filter #(= (-> % :wyId) 2057978) v)))
                  ((fn [v] (reduce (partial hash-by :wyId) (sorted-map) v)))))
 
 (def players (-> js/JSON
-                 (.parse (rc/inline "./data/players.json"))
+                 (.parse (rc/inline "../data/players.json"))
                  (js->clj :keywordize-keys true)
                  ((fn [v] (reduce (partial hash-by :wyId) (sorted-map) v)))))
 
@@ -19,7 +19,7 @@
   (let [group-by-id (fn [v] (group-by :teamId v))
         assoc-player-data #(assoc-in % [:playerData] (get-in players [(-> % :playerId)]))]
     (-> js/JSON
-        (.parse (rc/inline "./data/passes.json"))
+        (.parse (rc/inline "../data/passes.json"))
         (js->clj :keywordize-keys true)
         ((fn [p] (map assoc-player-data p)))
         group-by-id
