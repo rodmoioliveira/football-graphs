cffdaada2dab6edd8694cea9cbd34771493f8cc3
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-01-31 15:22:50 -0300

    spit jogos do Brasil

--- get_all_brazil_matches.sh
+++ get_all_brazil_matches.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for id in 2057978 2057980 2057982
+for id in 2057978 2057980 2057982 2058011 2058006
 do
   echo "Getting match $id"
   clj src/main/io/spit_match.clj --id=$id --type=edn
