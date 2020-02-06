e476bd5e29e3094224e575cbd3c2713b5a0b4fa2
rodmoioliveira <rodolfo.oliveira@tvglobo.com.br>
2020-02-06 02:12:31 -0300

    renomeia arquivo

--- install_python.sh
+++ install_python.sh
@@ -4,5 +4,5 @@ sudo apt install python3-venv
 pip install virtualenv
 virtualenv my-project-env
 source my-project-env/bin/activate
-pip install -r requirements.txt
+pip install -r req.txt
 export PYTHONPATH="$PWD/src/main/io"
