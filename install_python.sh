acde3c5a5f28e6a6f5fbf44ef2a979cec23ef832
Rodolfo Mói de Oliveira <rodmoi.oliveira@.gmail.com>
2020-02-18 17:49:11 -0300

    save manjaro config for streamline

--- install_python.sh
+++ install_python.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo apt install python3-pip
-sudo apt install python3-venv
-pip install virtualenv
-virtualenv my-project-env
-source my-project-env/bin/activate
+# sudo apt install python3-pip
+# sudo apt install python3-venv
+# pip install virtualenv
+# virtualenv my-project-env
+pyenv virtualenv my-project-env
+pyenv my-project-env
+# source my-project-env/bin/activate
 pip install -r req.txt
 export PYTHONPATH="$PWD/src/main/io"
