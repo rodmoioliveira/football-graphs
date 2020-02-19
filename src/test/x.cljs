1495fc5a1dbff359f6e4e3f626c55568543cc0e7
rodmoioliveira <rodmoi.oliveira@.gmail.com>
2020-02-19 01:11:48 -0300

    trying to extend testing to cljs

--- src/test/x.cljs
+++ src/test/x.cljs
@@ -1,10 +1,17 @@
-(ns test.core
-  (:require
-   [clojure.spec.test.alpha :as stest]
-   [clojure.set :refer [project]]
-   [clojure.pprint :refer [pprint]]
-   [utils.core]))
+; TODO: read this!
+; clj src/test/x.cljs
+; https://clojurescript.org/about/differences#_macros
+; https://bauerspace.com/refer-a-macro-with-cljc/
 
+(ns test.x
+  (:require
+   [cljs.spec.test.alpha :as stest :refer [check enumerate-namespace]]
+   ; [clojure.set :refer [project]]
+   ; [cljs.pprint :refer [pprint]]
+   ; [utils.core]
+   ))
+
+; TODO: automate clojurescript testing also...?
 (defn has-error?
   [results]
   (let [errors (reduce (fn [acc cur] (or (some? cur) acc)) false results)
@@ -17,7 +24,7 @@
         (println)
         (println (str num-errors " fail test" (when pluralize? "s")  "..."))
         (println)
-        (pprint results)
+        (println results)
         (println)
         (println "================================")
         (println)
@@ -27,12 +34,15 @@
         (println "All tests passed!")
         (System/exit 0)))))
 
-(-> (stest/enumerate-namespace 'utils.core)
+(->
+    (stest/enumerate-namespace 'utils.core)
     stest/check
     ((fn [tests] (map #(get-in % [:failure]) tests)))
     (#(filter some? %))
     (#(map Throwable->map %))
-    (#(project % [:cause]))
-    vec
-    has-error?)
+    ; (#(project % [:cause]))
+    ; vec
+    ; has-error?
+    print
+    )
 
