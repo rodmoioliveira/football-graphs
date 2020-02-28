51844b56459c6045d36abd916219c1d44f148a36
rodmoioliveira <rodmoi.oliveira@.gmail.com>
2020-02-27 22:13:19 -0300

    extract some more fns

--- src/main/football/draw_graph.cljs
+++ src/main/football/draw_graph.cljs
@@ -237,11 +237,11 @@
   (doto ctx
     (.beginPath)
     (.arc
-      (/ length 2)
-      (/ width 2)
-      (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
-      0
-      (* 2 js/Math.PI))
+     (/ length 2)
+     (/ width 2)
+     (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
+     0
+     (* 2 js/Math.PI))
     (.stroke)))
 
 (defn draw-midfield-point
@@ -251,6 +251,256 @@
     (.beginPath)
     (.arc (/ length 2) (/ width 2) midfield-point-radius 0 (* 2 js/Math.PI))
     (.fill)))
+
+(defn draw-goals-rects
+  "Draw all gol rect areas."
+  [ctx
+   {:keys [flip?
+           length
+           width
+           padding
+           gol-length
+           gol-area-length
+           penal-area-length
+           penal-area-width
+           gol-area-width]}]
+  (if flip?
+    (doto ctx
+      ; ==============
+      ; gol
+      ; ==============
+      (.beginPath)
+      (.rect (- (/ length 2) (/ width (* 2 gol-length))) 0 (/ width gol-length) padding)
+      (.stroke)
+
+      ; ==============
+      ; gol area
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- (/ length 2) (/ width (* 2 gol-area-length)))
+       padding
+       (/ width gol-area-length)
+       (/ width gol-area-width))
+      (.stroke)
+
+      ; ==============
+      ; penal area
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- (/ length 2) (/ width (* 2 penal-area-length)))
+       padding
+       (/ width penal-area-length)
+       (/ width penal-area-width))
+      (.stroke)
+
+      ; ==============
+      ; gol
+      ; ==============
+      (.beginPath)
+      (.rect (- (/ length 2) (/ width (* 2 gol-length))) (- width padding) (/ width gol-length) padding)
+      (.stroke)
+
+      ; ==============
+      ; gol area
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- (/ length 2) (/ width (* 2 gol-area-length)))
+       (- width padding (/ width gol-area-width))
+       (/ width gol-area-length)
+       (/ width gol-area-width))
+      (.stroke)
+
+      ; ==============
+      ; penal area
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- (/ length 2) (/ width (* 2 penal-area-length)))
+       (- width padding (/ width penal-area-width))
+       (/ width penal-area-length)
+       (/ width penal-area-width))
+      (.stroke))
+
+    (doto ctx
+      ; ==============
+      ; gol
+      ; ==============
+      (.beginPath)
+      (.rect
+       0
+       (- (/ width 2) (/ length (* 2 gol-length)))
+       padding
+       (/ length gol-length))
+      (.stroke)
+
+      ; ==============
+      ; gol area
+      ; ==============
+      (.beginPath)
+      (.rect
+       padding
+       (- (/ width 2) (/ length (* 2 gol-area-length)))
+       (/ length gol-area-width)
+       (/ length gol-area-length))
+      (.stroke)
+
+      ; ==============
+      ; penal area
+      ; ==============
+      (.beginPath)
+      (.rect
+       padding
+       (- (/ width 2) (/ length (* 2 penal-area-length)))
+       (/ length penal-area-width)
+       (/ length penal-area-length))
+      (.stroke)
+
+      ; ==============
+      ; gol
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- length padding)
+       (- (/ width 2) (/ length (* 2 gol-length)))
+       padding
+       (/ length gol-length))
+      (.stroke)
+
+      ; ==============
+      ; gol area
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- length padding (/ length gol-area-width))
+       (- (/ width 2) (/ length (* 2 gol-area-length)))
+       (/ length gol-area-width)
+       (/ length gol-area-length))
+      (.stroke)
+
+      ; ==============
+      ; penal area
+      ; ==============
+      (.beginPath)
+      (.rect
+       (- length padding (/ length penal-area-width))
+       (- (/ width 2) (/ length (* 2 penal-area-length)))
+       (/ length penal-area-width)
+       (/ length penal-area-length))
+      (.stroke))))
+
+(defn draw-penal-arcs
+  "Draw all penal arcs."
+  [ctx
+   {:keys [flip?
+           length
+           width
+           midfield-cicle-radius
+           midfield-point-radius]}]
+  (if flip?
+    (doto ctx
+      ; ==============
+      ; penal circle area
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 2)
+       (/ width 8.3)
+       (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
+       (* 0.2 js/Math.PI)
+       (* 0.8 js/Math.PI))
+      (.stroke)
+
+      ; ==============
+      ; penal circle point
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 2)
+       (/ width 8.3)
+       midfield-point-radius
+       0
+       (* 2 js/Math.PI))
+      (.stroke)
+      (.fill)
+
+      ; ==============
+      ; penal circle area
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 2)
+       (/ width 1.136)
+       (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
+       (* 1.205 js/Math.PI)
+       (/ js/Math.PI 0.557))
+      (.stroke)
+
+      ; ==============
+      ; penal circle point
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 2)
+       (/ width 1.136)
+       midfield-point-radius
+       0
+       (* 2 js/Math.PI))
+      (.stroke)
+      (.fill))
+
+    (doto ctx
+      ; ==============
+      ; penal circle area
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 8.3)
+       (/ width 2)
+       (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
+       (* 1.701 js/Math.PI)
+       (* 2.299 js/Math.PI))
+      (.stroke)
+
+      ; ==============
+      ; penal circle point
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 8.3)
+       (/ width 2)
+       midfield-point-radius
+       0
+       (* 2 js/Math.PI))
+      (.stroke)
+      (.fill)
+
+      ; ==============
+      ; penal circle area
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 1.136)
+       (/ width 2)
+       (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
+       (* 0.708 js/Math.PI)
+       (* 1.296 js/Math.PI))
+      (.stroke)
+
+      ; ==============
+      ; penal circle point
+      ; ==============
+      (.beginPath)
+      (.arc
+       (/ length 1.136)
+       (/ width 2)
+       midfield-point-radius
+       0
+       (* 2 js/Math.PI))
+      (.stroke)
+      (.fill))))
 
 (defn draw-field
   "Draw soccer field on canvas."
@@ -276,237 +526,22 @@
       (draw-borders padding width length)
       (draw-corners padding width length corner-radius)
       (draw-midfield-line padding width length flip?)
-
-      (#(if flip?
-          (doto %
-            ; ==============
-            ; gol
-            ; ==============
-            (.beginPath)
-            (.rect (- (/ length 2) (/ width (* 2 gol-length))) 0 (/ width gol-length) padding)
-            (.stroke)
-
-            ; ==============
-            ; gol area
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- (/ length 2) (/ width (* 2 gol-area-length)))
-             padding
-             (/ width gol-area-length)
-             (/ width gol-area-width))
-            (.stroke)
-
-            ; ==============
-            ; penal area
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- (/ length 2) (/ width (* 2 penal-area-length)))
-             padding
-             (/ width penal-area-length)
-             (/ width penal-area-width))
-            (.stroke)
-
-            ; ==============
-            ; gol
-            ; ==============
-            (.beginPath)
-            (.rect (- (/ length 2) (/ width (* 2 gol-length))) (- width padding) (/ width gol-length) padding)
-            (.stroke)
-
-            ; ==============
-            ; gol area
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- (/ length 2) (/ width (* 2 gol-area-length)))
-             (- width padding (/ width gol-area-width))
-             (/ width gol-area-length)
-             (/ width gol-area-width))
-            (.stroke)
-
-            ; ==============
-            ; penal area
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- (/ length 2) (/ width (* 2 penal-area-length)))
-             (- width padding (/ width penal-area-width))
-             (/ width penal-area-length)
-             (/ width penal-area-width))
-            (.stroke))
-
-          (doto %
-            ; ==============
-            ; gol
-            ; ==============
-            (.beginPath)
-            (.rect
-             0
-             (- (/ width 2) (/ length (* 2 gol-length)))
-             padding
-             (/ length gol-length))
-            (.stroke)
-
-            ; ==============
-            ; gol area
-            ; ==============
-            (.beginPath)
-            (.rect
-             padding
-             (- (/ width 2) (/ length (* 2 gol-area-length)))
-             (/ length gol-area-width)
-             (/ length gol-area-length))
-            (.stroke)
-
-            ; ==============
-            ; penal area
-            ; ==============
-            (.beginPath)
-            (.rect
-             padding
-             (- (/ width 2) (/ length (* 2 penal-area-length)))
-             (/ length penal-area-width)
-             (/ length penal-area-length))
-            (.stroke)
-
-            ; ==============
-            ; gol
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- length padding)
-             (- (/ width 2) (/ length (* 2 gol-length)))
-             padding
-             (/ length gol-length))
-            (.stroke)
-
-            ; ==============
-            ; gol area
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- length padding (/ length gol-area-width))
-             (- (/ width 2) (/ length (* 2 gol-area-length)))
-             (/ length gol-area-width)
-             (/ length gol-area-length))
-            (.stroke)
-
-            ; ==============
-            ; penal area
-            ; ==============
-            (.beginPath)
-            (.rect
-             (- length padding (/ length penal-area-width))
-             (- (/ width 2) (/ length (* 2 penal-area-length)))
-             (/ length penal-area-width)
-             (/ length penal-area-length))
-            (.stroke))))
-
-      (#(if flip?
-          (doto %
-            ; ==============
-            ; penal circle area
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 2)
-             (/ width 8.3)
-             (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
-             (* 0.2 js/Math.PI)
-             (* 0.8 js/Math.PI))
-            (.stroke)
-
-            ; ==============
-            ; penal circle point
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 2)
-             (/ width 8.3)
-             midfield-point-radius
-             0
-             (* 2 js/Math.PI))
-            (.stroke)
-            (.fill)
-
-            ; ==============
-            ; penal circle area
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 2)
-             (/ width 1.136)
-             (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
-             (* 1.205 js/Math.PI)
-             (/ js/Math.PI 0.557))
-            (.stroke)
-
-            ; ==============
-            ; penal circle point
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 2)
-             (/ width 1.136)
-             midfield-point-radius
-             0
-             (* 2 js/Math.PI))
-            (.stroke)
-            (.fill))
-
-          (doto %
-            ; ==============
-            ; penal circle area
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 8.3)
-             (/ width 2)
-             (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
-             (* 1.701 js/Math.PI)
-             (* 2.299 js/Math.PI))
-            (.stroke)
-
-            ; ==============
-            ; penal circle point
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 8.3)
-             (/ width 2)
-             midfield-point-radius
-             0
-             (* 2 js/Math.PI))
-            (.stroke)
-            (.fill)
-
-            ; ==============
-            ; penal circle area
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 1.136)
-             (/ width 2)
-             (if flip? (/ width midfield-cicle-radius) (/ length midfield-cicle-radius))
-             (* 0.708 js/Math.PI)
-             (* 1.296 js/Math.PI))
-            (.stroke)
-
-            ; ==============
-            ; penal circle point
-            ; ==============
-            (.beginPath)
-            (.arc
-             (/ length 1.136)
-             (/ width 2)
-             midfield-point-radius
-             0
-             (* 2 js/Math.PI))
-            (.stroke)
-            (.fill))))
-
+      (draw-goals-rects
+       {:flip? flip?
+        :length length
+        :width width
+        :padding padding
+        :gol-length gol-length
+        :gol-area-length gol-area-length
+        :penal-area-length penal-area-length
+        :penal-area-width penal-area-width
+        :gol-area-width gol-area-width})
+      (draw-penal-arcs
+       {:flip? flip?
+        :length length
+        :width width
+        :midfield-cicle-radius midfield-cicle-radius
+        :midfield-point-radius midfield-point-radius})
       (draw-midfield-circle width length flip? midfield-cicle-radius)
       (draw-midfield-point width length midfield-point-radius))))
 
