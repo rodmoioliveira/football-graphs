d849d253806827407b535d3d7af31e575458b130
rodmoioliveira <rodmoi.oliveira@.gmail.com>
2020-03-01 15:45:11 -0300

    format file

--- src/main/football/metrics_nav.cljs
+++ src/main/football/metrics_nav.cljs
@@ -13,16 +13,16 @@
         min-passses-input (-> js/document (.querySelector (str "[data-metric='min-passes-to-display']")))
         is-global? (fn [v] (= v :global))
         get-metrics (fn [] {:node-color-metric (-> node-color-select .-value keyword)
-                        :node-radius-metric (-> node-area-select .-value keyword)
-                        :position-metric (-> position-select .-value keyword)
-                        :min-passes-to-display (-> min-passses-input .-value int)
-                        :global-metrics? (-> coverage-select .-value keyword is-global?)})]
+                            :node-radius-metric (-> node-area-select .-value keyword)
+                            :position-metric (-> position-select .-value keyword)
+                            :min-passes-to-display (-> min-passses-input .-value int)
+                            :global-metrics? (-> coverage-select .-value keyword is-global?)})]
     (-> (rx/of
-          node-color-select
-          node-area-select
-          coverage-select
-          position-select
-          min-passses-input)
+         node-color-select
+         node-area-select
+         coverage-select
+         position-select
+         min-passses-input)
         (.pipe
          (rx-op/mergeMap #(-> (rx/fromEvent % "change")
                               (.pipe (rx-op/map get-metrics))))
@@ -42,15 +42,12 @@
          (rx-op/map (fn [] (-> breakpoint (.getBoundingClientRect) .-top (#(if (neg? %) 1 0)))))
          (rx-op/distinctUntilChanged))
         (.subscribe (fn [v]
-                        (do
-                          (-> menu (.setAttribute "data-sticky" v))))))
+                      (do
+                        (-> menu (.setAttribute "data-sticky" v))))))
     (-> activate-btn
         (rx/fromEvent "click")
         (.subscribe (fn [_] (-> nav (.setAttribute "data-active" 1)))))
 
     (-> deactivate-btn
         (rx/fromEvent "click")
-        (.subscribe (fn [_] (-> nav (.setAttribute "data-active" 0)))))
-
-
-    ))
+        (.subscribe (fn [_] (-> nav (.setAttribute "data-active" 0)))))))
