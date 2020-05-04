9ea52dc9c971aa216dbb9579bc1f29329626ddcb
rodmoioliveira <rodmoi.oliveira@gmail.com>
2020-05-04 01:38:01 -0300

    add json and edn to dist

--- build-scripts/copy.js
+++ build-scripts/copy.js
@@ -3,4 +3,5 @@ const fs = require('fs-extra');
 fs.copySync('public/index.html', 'dist/index.html');
 fs.copySync('public/fonts', 'dist/fonts');
 fs.copySync('public/img', 'dist/img');
+fs.copySync('src/main/data/analysis', 'dist/data');
 fs.copySync('public/manifest', 'dist/manifest');
