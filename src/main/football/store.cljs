16762cb6f303aee24fd2e59af37ad27565b60ee1
rodmoioliveira <rodmoi.oliveira@gmail.com>
2020-05-14 00:02:33 -0300

    create atom for all-simulations

--- src/main/football/store.cljs
+++ src/main/football/store.cljs
@@ -4,9 +4,16 @@
    [football.matches :refer [world-cup-matches-hash]]))
 
 (defonce initial-state (if (is-development?) (world-cup-matches-hash) {}))
-
 (defonce store (atom initial-state))
-
 (defn update-store
   [data]
   (swap! store merge {(-> data :match-id str keyword) data}))
+
+(defonce all-simulations (atom []))
+(defn flush-simulations!
+  []
+  (reset! all-simulations []))
+(defn stop-simulations
+  []
+  (doseq [simulation @all-simulations]
+    (-> simulation (.stop))))
