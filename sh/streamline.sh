ac201fd19ea4691f965421cadee8d23ccb8f4fd9
rodmoioliveira <rodmoi.oliveira@gmail.com>
2020-05-22 02:20:52 -0300

    generalize sh

---
+++ sh/streamline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. ./sh/match_ids.sh $1
+
+for id in $ids
+do
+  printf "===========================================================\n"
+  printf "Fetching match $id from dataset. Please wait...\n"
+  printf "===========================================================\n"
+  clj src/main/io/spit_match.clj --id=$id --type=edn --championship=$championship
+  clj src/main/io/spit_match.clj --id=$id --type=json --championship=$championship
+  printf "===========================================================\n"
+  printf "Creating graph...\n"
+  printf "===========================================================\n"
+  clj src/main/io/spit_graph.clj --id=$id --type=edn --championship=$championship
+  clj src/main/io/spit_graph.clj --id=$id --type=json --championship=$championship
+  printf "===========================================================\n"
+  printf "Calculating metrics...\n"
+  printf "This operations may take a while, please be patient...\n"
+  printf "===========================================================\n"
+  clj src/main/io/spit_graph_analysis.clj --id=$id --type=edn --championship=$championship
+  clj src/main/io/spit_graph_analysis.clj --id=$id --type=json --championship=$championship
+  printf "Done!\n"
+done
+
