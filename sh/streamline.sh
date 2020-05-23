614e68757dd895871f5f229392ec423339815d4d
rodmoioliveira <rodmoi.oliveira@gmail.com>
2020-05-23 13:52:58 -0300

    add second parameter to streamline

--- sh/streamline.sh
+++ sh/streamline.sh
@@ -2,30 +2,62 @@
 
 . ./sh/match_ids.sh $1
 
-for id in $ids
-do
+if [ $2 ]
+then
+  for id in $2
+  do
+    printf "===========================================================\n"
+    printf "Fetching match $id from dataset. Please wait...\n"
+    printf "===========================================================\n"
+    clj src/main/io/spit_match.clj --id=$id --type=edn --championship=$championship
+    clj src/main/io/spit_match.clj --id=$id --type=json --championship=$championship
+    printf "===========================================================\n"
+    printf "Creating graph...\n"
+    printf "===========================================================\n"
+    clj src/main/io/spit_graph.clj --id=$id --type=edn --championship=$championship
+    clj src/main/io/spit_graph.clj --id=$id --type=json --championship=$championship
+    printf "===========================================================\n"
+    printf "Calculating metrics...\n"
+    printf "This operations may take a while, please be patient...\n"
+    printf "===========================================================\n"
+    clj src/main/io/spit_graph_analysis.clj --id=$id --type=edn --championship=$championship
+    clj src/main/io/spit_graph_analysis.clj --id=$id --type=json --championship=$championship
+    printf "Done!\n"
+  done
   printf "===========================================================\n"
-  printf "Fetching match $id from dataset. Please wait...\n"
+  printf "Generating filenames... \n"
+  clj src/main/io/spit_filenames.clj
   printf "===========================================================\n"
-  clj src/main/io/spit_match.clj --id=$id --type=edn --championship=$championship
-  clj src/main/io/spit_match.clj --id=$id --type=json --championship=$championship
+  printf "Generating missing files... \n"
+  clj src/main/io/spit_missing.clj
   printf "===========================================================\n"
-  printf "Creating graph...\n"
+else
+  for id in $ids
+  do
+    printf "===========================================================\n"
+    printf "Fetching match $id from dataset. Please wait...\n"
+    printf "===========================================================\n"
+    clj src/main/io/spit_match.clj --id=$id --type=edn --championship=$championship
+    clj src/main/io/spit_match.clj --id=$id --type=json --championship=$championship
+    printf "===========================================================\n"
+    printf "Creating graph...\n"
+    printf "===========================================================\n"
+    clj src/main/io/spit_graph.clj --id=$id --type=edn --championship=$championship
+    clj src/main/io/spit_graph.clj --id=$id --type=json --championship=$championship
+    printf "===========================================================\n"
+    printf "Calculating metrics...\n"
+    printf "This operations may take a while, please be patient...\n"
+    printf "===========================================================\n"
+    clj src/main/io/spit_graph_analysis.clj --id=$id --type=edn --championship=$championship
+    clj src/main/io/spit_graph_analysis.clj --id=$id --type=json --championship=$championship
+    printf "Done!\n"
+  done
   printf "===========================================================\n"
-  clj src/main/io/spit_graph.clj --id=$id --type=edn --championship=$championship
-  clj src/main/io/spit_graph.clj --id=$id --type=json --championship=$championship
+  printf "Generating filenames... \n"
+  clj src/main/io/spit_filenames.clj
   printf "===========================================================\n"
-  printf "Calculating metrics...\n"
-  printf "This operations may take a while, please be patient...\n"
+  printf "Generating missing files... \n"
+  clj src/main/io/spit_missing.clj
   printf "===========================================================\n"
-  clj src/main/io/spit_graph_analysis.clj --id=$id --type=edn --championship=$championship
-  clj src/main/io/spit_graph_analysis.clj --id=$id --type=json --championship=$championship
-  printf "Done!\n"
-done
-printf "===========================================================\n"
-printf "Generating filenames... \n"
-clj src/main/io/spit_filenames.clj
-printf "===========================================================\n"
-printf "Generating missing files... \n"
-clj src/main/io/spit_missing.clj
-printf "===========================================================\n"
+fi
+
